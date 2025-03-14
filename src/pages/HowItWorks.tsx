7e1cfc002eddcb3a8366cd0da9a8adfd1db07d14
Hariksan <Hariksank.23@gmail.com>
2025-03-14 23:48:44 +0530

    set

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -15,12 +15,6 @@ const HowItWorksPage = () => {
       <div className="relative z-10">
         <Navbar />
         <div className="container mx-auto px-4 pt-24 pb-16">
-          <div className="text-center mb-12">
-            <h1 className="text-4xl font-bold mb-4 text-white drop-shadow-md">How It Works</h1>
-            <p className="text-xl max-w-2xl mx-auto text-white drop-shadow-md">
-              Our simple process connects you with skilled professionals in just a few steps.
-            </p>
-          </div>
           <HowItWorks />
         </div>
       </div>
